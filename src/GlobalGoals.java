b20af3080b74a2ab57a8609a20dc5aa414d43d1e
Victor hung <vhung@kth.se>
2014-08-09 17:08:36 +0200

    Added setModuleParams()-function with a TODO-mark.

--- src/GlobalGoals.java
+++ src/GlobalGoals.java
@@ -1,3 +1,9 @@
 public class GlobalGoals {
-  
+  public GlobalGoals(){
+
+  }
+
+  public setModuleParams(Module m){
+      // TODO: Takes a Module object and sets it initial values with the global goals in consideration.
+  }
 }
