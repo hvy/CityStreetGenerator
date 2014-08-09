a1eb2a6f6e11434215fcc02d6a6257dc8454d312
Victor hung <vhung@kth.se>
2014-08-09 17:33:58 +0200

    Added another TODO in GlobalGoals.

--- src/GlobalGoals.java
+++ src/GlobalGoals.java
@@ -1,9 +1,9 @@
 public class GlobalGoals {
   public GlobalGoals(){
-
+    // TODO: Initialise global goals from image?
   }
 
   public void setModuleParams(Module m){
-      // TODO: Takes a Module object and sets it initial values with the global goals in consideration.
+    // TODO: Takes a Module object and sets it initial values with the global goals in consideration.
   }
 }
