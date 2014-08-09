198fb2a051c05bf5deab0998b71dea368bc16654
Victor hung <vhung@kth.se>
2014-08-09 17:38:51 +0200

    Added todo-function that will check a modules params with the global goals.

--- src/GlobalGoals.java
+++ src/GlobalGoals.java
@@ -6,4 +6,9 @@ public class GlobalGoals {
   public void setModuleParams(Module m){
     // TODO: Takes a Module object and sets it initial values with the global goals in consideration.
   }
+
+  public boolean checkGlobalGoals(Module m){
+    // TODO: Takes a module (?) and checks its params with global goals.
+    return false;
+  }
 }
