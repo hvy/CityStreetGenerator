caa13457b62d6fb35e5a4e1f80e938654a392683
John Pertoft <john@pertoft.se>
2014-08-09 17:21:26 +0200

    Added processSegment function with TODO in LocalConstraint.

--- src/LocalConstraints.java
+++ src/LocalConstraints.java
@@ -1,5 +1,13 @@
 public class LocalConstraints {
 
+  public LocalConstraints() {
+    // TODO
+  }
+
+  public void processRoadSegment(RoadAttribute roadAttribute) {
+    // TODO
+  }
+
   private void pruneSegment(Road r){
     // TODO: Takes a road segment and prunes its length
   }
