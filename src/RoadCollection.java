438402dc4ff33a7d02ca52b76ad59cbac21061a0
Victor hung <vhung@kth.se>
2014-08-11 19:27:45 +0200

    Added new class RoadCollection for storing the roads

---
+++ src/RoadCollection.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * Created by vhung on 2014-08-11.
+ */
+public class RoadCollection {
+  private ArrayList<HashSet<Integer>> roads;
+
+  public RoadCollection(int imageWidth){
+    roads = new ArrayList<>(imageWidth);
+  }
+
+  public void addRoad(){
+    
+  }
+}
