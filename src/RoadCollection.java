5bfb37e3daa1b6c195a2abe2810d9e2048b7b2d5
Victor hung <vhung@kth.se>
2014-08-11 19:48:56 +0200

    Added basic functionality to RoadCollection

--- src/RoadCollection.java
+++ src/RoadCollection.java
@@ -1,17 +1,24 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 
-/**
- * Created by vhung on 2014-08-11.
- */
 public class RoadCollection {
-  private ArrayList<HashSet<Integer>> roads;
+  private ArrayList<HashSet<RoadAttribute>> roads;
 
   public RoadCollection(int imageWidth){
     roads = new ArrayList<>(imageWidth);
+    for(int i = 0; i < roads.size(); i++){
+      roads.add(i, new HashSet<RoadAttribute>());
+    }
   }
 
-  public void addRoad(){
-    
+  public void addRoad(RoadAttribute r){
+    if(!roads.get(r.xpos).contains(r)){
+      roads.get(r.xpos).add(r);
+    }
+  }
+
+  public boolean checkRoad(RoadAttribute r){
+    // TODO
+    return false;
   }
 }
