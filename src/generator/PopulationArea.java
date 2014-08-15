8be1574c7e37f307f63998a0638acf5071f28bfa
Victor hung <vhung@kth.se>
2014-08-15 15:15:38 +0200

    New class PopulationArea
    Created new class that holds information about population areas. Holds
    their x,y-coordinates as well as the size

---
+++ src/generator/PopulationArea.java
@@ -0,0 +1,16 @@
+package generator;
+
+/**
+ * Created by vhung on 2014-08-15.
+ */
+public class PopulationArea {
+  int xPos;
+  int yPos;
+  int width;
+
+  public PopulationArea(int xPos, int yPos, int width){
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.width = width;
+  }
+}
