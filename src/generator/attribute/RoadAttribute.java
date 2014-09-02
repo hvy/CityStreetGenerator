904615da43d11bb9717a7a9039ae21ffc2aa2df9
Victor hung <vhung@kth.se>
2014-09-02 15:17:57 +0200

    Updated RoadAttribute
    RoadAttribute now uses the generator.model.Pos to store (x,y)
    coordinates and now also holds the end point.

--- src/generator/attribute/RoadAttribute.java
+++ src/generator/attribute/RoadAttribute.java
@@ -1,4 +1,7 @@
 package generator.attribute;
+import generator.model.Pos;
+import generator.model.Road;
+import java.lang.Math;
 
 public class RoadAttribute {
   public final static int PRIMARY = 0;
@@ -7,9 +10,22 @@ public class RoadAttribute {
 
   public double angle;
   public int length;
-  public int xpos;
-  public int ypos;
+  public Pos startPos;
+  public Pos endPos;
   public int type;
+  public Road r;
+
+  public RoadAttribute(double angle, int length, Pos startPos, int type, Road r){
+    this.angle = angle;
+    this.length = length;
+    this.startPos = startPos;
+    this.type = type;
+    this.r = r;
+
+    int x = (int) Math.round(Math.cos(angle) * (double)length);
+    int y = (int) Math.round(Math.sin(angle) * (double)length);
+    endPos = new Pos(x,y);
+  }
 
   @Override
   public boolean equals(Object o) {
@@ -21,8 +37,8 @@ public class RoadAttribute {
     if (Double.compare(that.angle, angle) != 0) return false;
     if (length != that.length) return false;
     if (type != that.type) return false;
-    if (xpos != that.xpos) return false;
-    if (ypos != that.ypos) return false;
+    if (startPos != that.startPos) return false;
+    if (endPos != that.endPos) return false;
 
     return true;
   }
@@ -34,8 +50,10 @@ public class RoadAttribute {
     temp = Double.doubleToLongBits(angle);
     result = (int) (temp ^ (temp >>> 32));
     result = 31 * result + length;
-    result = 31 * result + xpos;
-    result = 31 * result + ypos;
+    result = 31 * result + startPos.getX();
+    result = 31 * result + startPos.getY();
+    result = 31 * result + endPos.getX();
+    result = 31 * result + endPos.getY();
     result = 31 * result + type;
     return result;
   }
